a4598b631a325b5ae16491d776a137765ca0785a
sorajiro-33 <tasm,yuryo0425@gmail.com>
2025-04-30 16:38:25 +0900

    いらない記号を消す

--- db_lesson.sql
+++ db_lesson.sql
@@ -1,11 +1,11 @@
 Q1:
 CREATE TABLE 
    `departments`(
-     -> department_id int unsigned auto_increment primary key NOT NULL,
-     -> name VARCHAR(20) NOT NULL,
-     -> created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-     -> updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-     -> );
+     department_id int unsigned auto_increment primary key NOT NULL,
+     name VARCHAR(20) NOT NULL,
+     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+     );
 
 Q2:
 ALTER TABLE 
@@ -21,42 +21,42 @@ Q3:
 //追加する部署一覧//
 INSERT INTO 
    departments (name)
-    -> VALUES
-    -> ('営業'),
-    -> ('開発'),
-    -> ('経理'),
-    -> ('人事'),
-    -> ('情報システム');
+     VALUES
+     ('営業'),
+     ('開発'),
+     ('経理'),
+     ('人事'),
+     ('情報システム');
 
 //追加する人の条件//
 INSERT INTO 
     people (name, email, age, gender,department_id)
-    -> VALUES
-    -> ('小林拓郎', 'kobayashi@beyond-works.co.jp', 33, 1, 1),
-    -> ('佐川美琴', 'sagawa@beyond-works.co.jp', 56, 2, 1),
-    -> ('井野辺ゆう', 'inobe@beyond-works.co.jp', 26, 2, 1),
-    -> ('田上司', 'tagami@beyond-works.co.jp', 45, 1, 2),
-    -> ('柿原裕二', 'kakihara@beyond-works.co.jp', 61, 1, 2),
-    -> ('早乙女朱里', 'saotome@beyond-works.co.jp', 33, 2, 2),
-    -> ('河野すず', 'kawano@beyond-works.co.jp', 37, 2, 2),
-    -> ('榎本恵理', 'enomoto@beyond-works.co.jp', 42, 2, 3),
-    -> ('岡部剛', 'okabe@beyond-works.co.jp', 25, 1, 4),
-    -> ('畠山重幸', 'hatakeyama@beyond-works.co.jp', 38, 1, 5);
+     VALUES
+     ('小林拓郎', 'kobayashi@beyond-works.co.jp', 33, 1, 1),
+     ('佐川美琴', 'sagawa@beyond-works.co.jp', 56, 2, 1),
+     ('井野辺ゆう', 'inobe@beyond-works.co.jp', 26, 2, 1),
+     ('田上司', 'tagami@beyond-works.co.jp', 45, 1, 2),
+     ('柿原裕二', 'kakihara@beyond-works.co.jp', 61, 1, 2),
+     ('早乙女朱里', 'saotome@beyond-works.co.jp', 33, 2, 2),
+     ('河野すず', 'kawano@beyond-works.co.jp', 37, 2, 2),
+     ('榎本恵理', 'enomoto@beyond-works.co.jp', 42, 2, 3),
+     ('岡部剛', 'okabe@beyond-works.co.jp', 25, 1, 4),
+     ('畠山重幸', 'hatakeyama@beyond-works.co.jp', 38, 1, 5);
 
 //追加する日報の条件//
 INSERT INTO 
     reports (person_id, content)
-    -> VALUES
-    -> (1, 'ああああああああああ'),
-    -> (1, 'ええええええええええええええ'),
-    -> (1, 'そそそそそそそそそそそ'),
-    -> (2, 'にこにこにこにこにこにこ'),
-    -> (3, 'ままままっまっままままままっまっま'),
-    -> (3, 'おやおやおやおやおやおやおやおやおや'),
-    -> (4, 'だだだだだだだだだだだだ'),
-    -> (4, 'すすすすすすすすすすすすすす'),
-    -> (4, 'ふふふふふふふふふふふふうふふ'),
-    -> (6, 'ここここおこここおここここここここ');
+     VALUES
+     (1, 'ああああああああああ'),
+     (1, 'ええええええええええええええ'),
+     (1, 'そそそそそそそそそそそ'),
+     (2, 'にこにこにこにこにこにこ'),
+     (3, 'ままままっまっままままままっまっま'),
+     (3, 'おやおやおやおやおやおやおやおやおや'),
+     (4, 'だだだだだだだだだだだだ'),
+     (4, 'すすすすすすすすすすすすすす'),
+     (4, 'ふふふふふふふふふふふふうふふ'),
+     (6, 'ここここおこここおここここここここ');
 
 Q4:
 UPDATE 
@@ -64,35 +64,35 @@ UPDATE
 SET 
    department_id = 1 
 WHERE 
-   person_id = '1';
+   person_id = 1;
 
 UPDATE 
    people 
 SET 
    department_id = 2 
 WHERE 
-   person_id = '2';
+   person_id = 2;
 
 UPDATE 
    people 
 SET 
    department_id = 3 
 WHERE 
-   person_id = '3';
+   person_id = 3;
 
 UPDATE 
    people 
 SET 
    department_id = 4 
 WHERE 
-   person_id = '4';
+   person_id = 4;
 
 UPDATE 
    people 
 SET 
    department_id = 6 
 WHERE 
-   person_id = '5';
+   person_id = 5;
 
 Q5:
 SELECT 
@@ -100,7 +100,7 @@ SELECT
 FROM 
    people 
 WHERE 
-   gender = '1' 
+   gender = 1 
 ORDER BY age DESC;
 
 Q6:テーブル・レコード・カラムという3つの単語を適切に使用して、下記のSQL文を日本語で説明してください。
@@ -113,11 +113,11 @@ FROM
    people 
 WHERE 
    gender = 2 
-AND 
+   AND 
    age BETWEEN 20 and 29 
 OR 
    gender = 1 
-AND 
+   AND 
    age BETWEEN 40 and 49;
 
 Q8:
@@ -139,7 +139,7 @@ FROM
    people 
 WHERE 
    gender = 2 
-AND 
+   AND 
    department_id = 2;
 
 Q10:
